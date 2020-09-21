930cb7d70ec4e9c81eb47abc63b89d29c77266b0
Minh Bui <tranduyminh.bui@gmail.com>
2020-09-21 12:14:20 +1000

    try basic and better approach for directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,21 @@ import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 import { ServerComponent } from "./server/server.component";
 import { ServersComponent } from "./servers/servers.component";
-import { CockpitComponent } from './cockpit/cockpit.component';
-import { ServerElementComponent } from './server-element/server-element.component';
+import { CockpitComponent } from "./cockpit/cockpit.component";
+import { ServerElementComponent } from "./server-element/server-element.component";
+import { BasicHighlightDirective } from "./basic-highlight/basic-highlight.directive";
+import { BetterHighlightDirective } from "./better-highlight/better-highlight.directive";
 
 @NgModule({
-  declarations: [AppComponent, ServerComponent, ServersComponent, CockpitComponent, ServerElementComponent],
+  declarations: [
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    CockpitComponent,
+    ServerElementComponent,
+    BasicHighlightDirective,
+    BetterHighlightDirective,
+  ],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
